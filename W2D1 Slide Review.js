e089cbe660d37097b78047a6d6594c2bb49dd52a
Scott Nelson <scottninlv@yahoo.com>
2023-07-16 21:20:18 -0700

    changing a few lines

---
+++ W2D1 Slide Review.js
@@ -0,0 +1,21 @@
+let num = 0 
+let newNum = Boolean(num)
+console.log(newNum)
+
+let name = "Scott"
+let newName = Boolean(name)
+console.log(newName)
+
+let myString = "Las Vegas"
+
+console.log(myString.includes("eg"))
+console.log(myString.includes("V"))
+console.log(myString.includes("s"))
+
+let crazyString = "ThIS iS A CRazy StrinG"
+
+let upperCrazy = crazyString.toUpperCase()
+let lowerCrazy = crazyString.toLowerCase()
+
+console.log(upperCrazy)
+console.log(lowerCrazy)
--- testReview.js
+++ testReview.js
@@ -1,75 +1,75 @@
-// console.log("Hello World!")
+console.log("Hello World!")
 
 
-// // simple function to log out someones name
-// function firstName(name) {
-//     console.log(name)
-// }
+// simple function to log out someones name
+function firstName(name) {
+    console.log(name)
+}
 
-// firstName("Scott")
+firstName("Scott")
 
-// //function that will log out a random number
-// function randomNumber() {
-//     console.log(Math.floor(Math.random() * 50) + 1)
-// }
+//function that will log out a random number
+function randomNumber() {
+    console.log(Math.floor(Math.random() * 50) + 1)
+}
 
-// randomNumber()
+randomNumber()
 
 
-// // Function that takes the random number and returns a students grade
-// let studentGrade = Math.floor(Math.random() * 100) + 1
+// Function that takes the random number and returns a students grade
+let studentGrade = Math.floor(Math.random() * 100) + 1
 
-//  function calculateScore() {
-//     if(studentGrade >= 90) {
-//         return "Your grade is an A!"
-//     } else if(studentGrade < 90 && studentGrade >= 80) {
-//         return "Your grade is a B"
-//     } else if(studentGrade < 80 && studentGrade >= 70) {
-//         return "Your grade is a C"
-//     } else if(studentGrade < 70 && studentGrade >= 60) {
-//         return "Your grade is a D"
-//     } else {
-//         return "Your grade is an F"
-//     }
-//  }
+ function calculateScore() {
+    if(studentGrade >= 90) {
+        return "Your grade is an A!"
+    } else if(studentGrade < 90 && studentGrade >= 80) {
+        return "Your grade is a B"
+    } else if(studentGrade < 80 && studentGrade >= 70) {
+        return "Your grade is a C"
+    } else if(studentGrade < 70 && studentGrade >= 60) {
+        return "Your grade is a D"
+    } else {
+        return "Your grade is an F"
+    }
+ }
 
-//  console.log(calculateScore(studentGrade))
+ console.log(calculateScore(studentGrade))
 
-//  function multiplyNum(a, b) {   
-//         return a * b
-//  }
+ function multiplyNum(a, b) {   
+        return a * b
+ }
 
-//  console.log(multiplyNum(2, 2))
+ console.log(multiplyNum(2, 2))
  
-// // simple function to greet someone
-// function greet(name) {
-//     return `Hello ${name}, how are you?`
-// }
+// simple function to greet someone
+function greet(name) {
+    return `Hello ${name}, how are you?`
+}
 
-// console.log(greet("Tim"))
+console.log(greet("Tim"))
 
-// // function to find the average
-// function findAverage(a, b, c, d) {
-//     let num = a + b + c + d
-//     return num / 4
-// }
+// function to find the average
+function findAverage(a, b, c, d) {
+    let num = a + b + c + d
+    return num / 4
+}
 
-// console.log(findAverage(5,3,9,6))
+console.log(findAverage(5,3,9,6))
 
-// function greeting2(firstName, lastName) {
-//     return `Hello ${firstName,lastName}, what are you doing tonight?`
-// }
+function greeting2(firstName, lastName) {
+    return `Hello ${firstName,lastName}, what are you doing tonight?`
+}
 
-// console.log(greeting2("Scott", "Nelson"))
+console.log(greeting2("Scott", "Nelson"))
 
-// function repeat(msg, reps) {
-//      for (let i = 0; i < reps; i++) {
-//         console.log("Hi")
-//     }
+function repeat(msg, reps) {
+     for (let i = 0; i < reps; i++) {
+        console.log("Hi")
+    }
    
-// }
+}
 
-// repeat("Hi", 5)
+repeat("Hi", 5)
 
 function add(x, y) {
     if(typeof x !== "number" || typeof y !== "number") {
